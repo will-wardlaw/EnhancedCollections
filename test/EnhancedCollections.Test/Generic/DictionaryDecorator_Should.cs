a24ed54c59d73cc11556bda8644a10fab98d53fc
will-wardlaw <will-wardlaw@users.noreply.github.com>
2016-11-12 17:54:43 -0700

    Added DictionaryDecorator_Should class

---
+++ test/EnhancedCollections.Test/Generic/DictionaryDecorator_Should.cs
@@ -0,0 +1,14 @@
+
+using Xunit;
+
+namespace EnhancedCollections.Test.Generic
+{
+    public class DictionaryDecorator_Should
+    {
+        [Fact]
+        public void CallDecoratedIndexer()
+        {
+            
+        }
+    }
+}
