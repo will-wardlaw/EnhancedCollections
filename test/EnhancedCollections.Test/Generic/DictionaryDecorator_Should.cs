5ba35ab45d1abebb27a1ccc99ae2ce1cdbd0e9f8
will-wardlaw <will-wardlaw@users.noreply.github.com>
2016-11-19 19:40:59 -0700

    Fixed some of the misuse of Moq.  More Unit tests pass.

--- test/EnhancedCollections.Test/Generic/DictionaryDecorator_Should.cs
+++ test/EnhancedCollections.Test/Generic/DictionaryDecorator_Should.cs
@@ -13,7 +13,7 @@ namespace EnhancedCollections.Test.Generic
     {
         private Mock<IDictionary<T, V>> GetMock<T, V>()
         {
-            return new Mock<IDictionary<T, V>>();
+            return new Mock<IDictionary<T, V>>( MockBehavior.Loose);
         }
 
         private DictionaryDecorator<T, V> GetDecorator<T, V>( IDictionary<T, V> decorated)
@@ -76,73 +76,73 @@ namespace EnhancedCollections.Test.Generic
         [Fact]
         public void CallDecoratedAddWithKvp()
         {
-            CallDecoratedMethod<int, int>( d => d.Add( new KeyValuePair<int, int>( 5, 5)));
+            CallDecoratedVoidMethod<int, int>( d => d.Add( new KeyValuePair<int, int>( 5, 5)));
         }
 
         [Fact]
         public void CallDecoratedAdd()
         {
-            CallDecoratedMethod<int, int>( d => d.Add( 5, 5));
+            CallDecoratedVoidMethod<int, int>( d => d.Add( 5, 5));
         }
 
         [Fact]
         public void CallClear()
         {
-            CallDecoratedMethod<int, int>( d => d.Clear());
+            CallDecoratedVoidMethod<int, int>( d => d.Clear());
         }
 
         [Fact]
         public void CallDecoratedContains()
         {
-            CallDecoratedMethod<int, int>( d =>  d.Contains( new KeyValuePair<int, int>( 5, 5)));
+            CallDecoratedMethod<int, int, bool>( d =>  d.Contains( new KeyValuePair<int, int>( 5, 5)));
         }
 
         [Fact]
         public void CallDecoratedContainsKey()
         {
-            CallDecoratedMethod<int, int>( d => d.ContainsKey( 5));
+            CallDecoratedMethod<int, int, bool>( d => d.ContainsKey( 5));
         }
 
         [Fact]
         public void CallDecoratedCopyTo()
         {
-            CallDecoratedMethod<int, int>( d => d.CopyTo( new KeyValuePair<int, int>[] {}, 0));
+            CallDecoratedVoidMethod<int, int>( d => d.CopyTo( new KeyValuePair<int, int>[] {}, 0));
         }
 
         [Fact]
         public void CallDecoratedGetEnumerator()
         {
-            CallDecoratedMethod<int, int>( d => d.GetEnumerator());
+            CallDecoratedVoidMethod<int, int>( d => d.GetEnumerator());
         }
 
         [Fact]
         public void CallDecoratedRemoveKvp()
         {
-            CallDecoratedMethod<int, int>( d => d.Remove( new KeyValuePair<int, int>( 5, 5)));
+            CallDecoratedMethod<int, int, bool>( d => d.Remove( new KeyValuePair<int, int>( 5, 5)));
         }
 
         [Fact]
         public void CallDecoratedRemove()
         {
-            CallDecoratedMethod< int, int>( d => d.Remove( 5));
+            CallDecoratedMethod< int, int, bool>( d => d.Remove( 5));
         }
 
         [Fact]
         public void CallDecoratedTryGetValue()
         {
             int outVal;
-            CallDecoratedMethod< int, int>( d => d.TryGetValue( 5, out outVal));
+            CallDecoratedMethod< int, int, bool>( d => d.TryGetValue( 5, out outVal));
         }
 
         [Fact]
         public void CallDecoratedNonGenericGetEnumerator()
         {
-            CallDecoratedMethod< int, int>( d => (d as IEnumerable).GetEnumerator());
+            CallDecoratedMethod< int, int, IEnumerator>( d => (d as IEnumerable).GetEnumerator());
         }
         
 
         
-        private void CallDecoratedMethod<TKey, TValue>( Expression<Action<IDictionary<TKey, TValue>>> expr)
+        private void CallDecoratedVoidMethod<TKey, TValue>( Expression<Action<IDictionary<TKey, TValue>>> expr)
         {
             var mock = GetMock<TKey, TValue>();
             
@@ -151,7 +151,22 @@ namespace EnhancedCollections.Test.Generic
             var decorator = GetDecorator( mock.Object);
 
             var compiled = expr.Compile();
-            var val = compiled.DynamicInvoke( decorator);
+
+            compiled.Invoke( decorator);
+
+            mock.Verify( expr);
+        }
+
+        private void CallDecoratedMethod<TKey, TValue, TRet>( Expression<Func<IDictionary<TKey, TValue>, TRet>> expr)
+        {
+            var mock = GetMock<TKey, TValue>();
+
+            mock.Setup( expr);
+
+            var decorator = GetDecorator( mock.Object);
+            var compiled = expr.Compile();
+
+            compiled.Invoke( decorator);
 
             mock.Verify( expr);
         }
@@ -165,6 +180,7 @@ namespace EnhancedCollections.Test.Generic
             var decorator = GetDecorator( mock.Object);
 
             var compiled = getterExpression.Compile();
+
             var val = compiled.DynamicInvoke( decorator);
 
             mock.VerifyGet( getterExpression);
