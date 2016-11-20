ae890ccb3b8317f463154c4c081856acb00554a3
will-wardlaw <william.wardlaw@gmail.com>
2016-11-19 21:09:05 -0700

    Dictionary decorator (#1)
    * Created DictionaryDecorator class
    
    * Added _decorated member and Constructor to DictionaryDecorator class.
    
    * Added DictionaryDecorator_Should class
    
    * Changed type of DictionaryDecorator._decorated to a generic type.
    
    * Fixed broken reference with Moq
    
    * Added the DictionaryDecorator_Should class
    
    * Minor change to call SetupGet() instead of Setup() in DictionaryDecorator_Should.CallDecoratorIndexer().
    
    * Changed name of DictionaryDecorator_Should.CallDecoratedIndexer() to CallDecoratedIndexerGet()
    
    * Implemented DictionaryDecorator indexer getter
    
    * Added DictionaryDecorator_Should.CallDecoratedIndexerSet() test
    
    * Changed call to Verify() in DictionaryDecorator_Should.CallDecoratedIndexerGet() to a call to VerifyGet().
    
    * Added a DictionaryDecorator_Should.CallDecoratedCount() test method.
    
    * Refactoring some testing code.
    
    * Refactoring of testing methods.
    
    * Added a number of test methods to DictionaryDecorator_Should class
    
    * Rough implementation.
    
    * Made public methods and properties virtual on DictionaryDecorator class.
    
    * Fixed some of the misuse of Moq.  More Unit tests pass.
    
    * Hard-coded values in unit test to get it to pass.  I still don't understand Moq enough.
    
    * Making sure I'm calling the decorated IEnumerable.GetEnumerator() method.
    
    * Using Moq's .As() to properly test IEnumerable.GetIEnumerator()

---
+++ src/EnhancedCollections/Generic/DictionaryDecorator.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EnhancedCollections.Generic
+{
+    public class DictionaryDecorator<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+
+        protected IDictionary<TKey, TValue> _decorated;
+
+        public DictionaryDecorator( IDictionary<TKey, TValue> decorated)
+        {
+            _decorated = decorated; 
+        }
+
+        public virtual TValue this[TKey key]
+        {
+            get
+            {
+                return _decorated[key];
+            }
+
+            set
+            {
+                _decorated[key] = value;
+            }
+        }
+
+        public virtual int Count
+        {
+            get
+            {
+                return _decorated.Count;
+            }
+        }
+
+        public virtual bool IsReadOnly
+        {
+            get
+            {
+                return _decorated.IsReadOnly;
+            }
+        }
+
+        public virtual ICollection<TKey> Keys
+        {
+            get
+            {
+                return _decorated.Keys;
+            }
+        }
+
+        public virtual ICollection<TValue> Values
+        {
+            get
+            {
+                return _decorated.Values;
+            }
+        }
+
+        public virtual void Add(KeyValuePair<TKey, TValue> item)
+        {
+            _decorated.Add( item);
+        }
+
+        public virtual void Add(TKey key, TValue value)
+        {
+            _decorated.Add( key, value);
+        }
+
+        public virtual void Clear()
+        {
+            _decorated.Clear();
+        }
+
+        public virtual bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _decorated.Contains( item);
+        }
+
+        public virtual bool ContainsKey(TKey key)
+        {
+            return _decorated.ContainsKey( key);
+        }
+
+        public virtual void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            _decorated.CopyTo( array, arrayIndex);
+        }
+
+        public virtual IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _decorated.GetEnumerator();
+        }
+
+        public virtual bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return _decorated.Remove(item);
+        }
+
+        public virtual bool Remove(TKey key)
+        {
+            return _decorated.Remove( key);
+        }
+
+        public virtual bool TryGetValue(TKey key, out TValue value)
+        {
+            return _decorated.TryGetValue( key, out value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (_decorated as IEnumerable).GetEnumerator();
+        }
+    }
+}
---
+++ test/EnhancedCollections.Test/Generic/DictionaryDecorator_Should.cs
@@ -0,0 +1,199 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using EnhancedCollections.Generic;
+using Moq;
+using Xunit;
+
+namespace EnhancedCollections.Test.Generic
+{
+    public class DictionaryDecorator_Should
+    {
+        private Mock<IDictionary<T, V>> GetMock<T, V>()
+        {
+            return new Mock<IDictionary<T, V>>( MockBehavior.Loose);
+        }
+
+        private DictionaryDecorator<T, V> GetDecorator<T, V>( IDictionary<T, V> decorated)
+        {
+            return new DictionaryDecorator<T, V>( decorated);
+        }
+
+        [Fact]
+        public void CallDecoratedIndexerGet()
+        {
+            var mock = GetMock<int, int>();
+
+            mock.SetupGet( m => m[It.IsAny<int>()]);
+
+            var decorator = GetDecorator( mock.Object);
+
+            int val = decorator[5];
+
+            mock.Verify( m => m[It.IsAny<int>()], Times.Once);
+        }
+
+        [Fact]
+        public void CallDecoratedIndexerSet()
+        {
+            var mock = GetMock<int, int>();
+
+            mock.SetupSet( m => m[5] = 5);
+
+            var decorator = GetDecorator( mock.Object);
+
+            decorator[5] = 5;
+
+            mock.VerifySet( m => m[5] = 5, Times.Once);
+        }
+
+        [Fact]
+        public void CallDecoratedCount()
+        {
+            CallDecoratedGetter<int, int, int>( m => m.Count);
+        }
+
+        [Fact]
+        public void CallDecoratedIsReadOnly()
+        {
+            CallDecoratedGetter<int, int, bool>( m => m.IsReadOnly);
+        }
+
+        [Fact]
+        public void CallDecoratedKeys()
+        {
+            CallDecoratedGetter<int, int, ICollection<int>>( m => m.Keys);
+        }
+
+        [Fact]
+        public void CallDecoratedValues()
+        {
+            CallDecoratedGetter<int, int, ICollection<int>>( m => m.Keys);
+        }
+
+        [Fact]
+        public void CallDecoratedAddWithKvp()
+        {
+            CallDecoratedVoidMethod<int, int>( d => d.Add( new KeyValuePair<int, int>( 5, 5)));
+        }
+
+        [Fact]
+        public void CallDecoratedAdd()
+        {
+            CallDecoratedVoidMethod<int, int>( d => d.Add( 5, 5));
+        }
+
+        [Fact]
+        public void CallClear()
+        {
+            CallDecoratedVoidMethod<int, int>( d => d.Clear());
+        }
+
+        [Fact]
+        public void CallDecoratedContains()
+        {
+            CallDecoratedMethod<int, int, bool>( d =>  d.Contains( new KeyValuePair<int, int>( 5, 5)));
+        }
+
+        [Fact]
+        public void CallDecoratedContainsKey()
+        {
+            CallDecoratedMethod<int, int, bool>( d => d.ContainsKey( 5));
+        }
+
+        [Fact]
+        public void CallDecoratedCopyTo()
+        {
+            CallDecoratedVoidMethod<int, int>( d => d.CopyTo( new KeyValuePair<int, int>[] {}, 0));
+        }
+
+        [Fact]
+        public void CallDecoratedGetEnumerator()
+        {
+            CallDecoratedVoidMethod<int, int>( d => d.GetEnumerator());
+        }
+
+        [Fact]
+        public void CallDecoratedRemoveKvp()
+        {
+            CallDecoratedMethod<int, int, bool>( d => d.Remove( new KeyValuePair<int, int>( 5, 5)));
+        }
+
+        [Fact]
+        public void CallDecoratedRemove()
+        {
+            CallDecoratedMethod< int, int, bool>( d => d.Remove( 5));
+        }
+
+        [Fact]
+        public void CallDecoratedTryGetValue()
+        {
+            int outVal;
+            CallDecoratedMethod< int, int, bool>( d => d.TryGetValue( 5, out outVal));
+        }
+
+        [Fact]
+        public void CallDecoratedNonGenericGetEnumerator()
+        {
+            var mock = GetMock<int, int>();
+
+            var nonGeneric = mock.As<IEnumerable>();
+
+            nonGeneric.Setup( d => d.GetEnumerator());
+
+            var decorator = GetDecorator( mock.Object);
+
+            (decorator as IEnumerable).GetEnumerator();
+
+            nonGeneric.Verify( d => d.GetEnumerator());
+        }
+        
+
+        
+        private void CallDecoratedVoidMethod<TKey, TValue>( Expression<Action<IDictionary<TKey, TValue>>> expr)
+        {
+            var mock = GetMock<TKey, TValue>();
+            
+            mock.Setup( expr);
+
+            var decorator = GetDecorator( mock.Object);
+
+            var compiled = expr.Compile();
+
+            compiled.Invoke( decorator);
+
+            mock.Verify( expr);
+        }
+
+        private void CallDecoratedMethod<TKey, TValue, TRet>( Expression<Func<IDictionary<TKey, TValue>, TRet>> expr)
+        {
+            var mock = GetMock<TKey, TValue>();
+
+            mock.Setup( expr);
+
+            var decorator = GetDecorator( mock.Object);
+            var compiled = expr.Compile();
+
+            compiled.Invoke( decorator);
+
+            mock.Verify( expr);
+        }
+        
+        private void CallDecoratedGetter<TKey, TValue, TProperty>( Expression<Func<IDictionary<TKey, TValue>, TProperty>> getterExpression)
+        {
+            var mock = GetMock<TKey, TValue>();
+
+            mock.SetupGet( getterExpression);
+
+            var decorator = GetDecorator( mock.Object);
+
+            var compiled = getterExpression.Compile();
+
+            var val = compiled.DynamicInvoke( decorator);
+
+            mock.VerifyGet( getterExpression);
+        }
+    }
+}
